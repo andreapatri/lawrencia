8bf78786fcc7919e7af1d61cac458d6a6c23dfef
andreapatri <andrea_patri@hotmail.com>
2020-08-09 11:48:34 -0500

    Add files via upload

---
+++ ultrasonic.ino
@@ -0,0 +1,39 @@
+int trigPin = 12;    // Trigger pin which sends the wave from the Transmitter end 
+int echoPin = 11;    // Echo pin which receives the wave at the Receiver end
+long duration, cm;
+ 
+void setup() {
+  //Serial Port begin
+  Serial.begin (9600);
+  //Define inputs and outputs
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+ 
+void loop() {
+  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
+  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+ 
+  // Read the signal from the sensor: a HIGH pulse whose
+  // duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+  duration = pulseIn(echoPin, HIGH);
+ 
+  // Convert the time into a distance
+  cm =  (duration * 0.0343) / 2 ;     // Divide by 29.1 or multiply by 0.0343
+  
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+  
+  delay(250);
+}
+
+
+
+
